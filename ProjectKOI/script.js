a9cf64b681540e78232b3f8e2b46b891a13a69a5
Dollazin <110367980+Dollazin@users.noreply.github.com>
2023-06-17 20:13:21 -0300

    Update script.js

--- ProjectKOI/script.js
+++ ProjectKOI/script.js
@@ -1,8 +1,9 @@
 let OptNames = [ "combo_sushi", "combo_pudim", "combo_temaki", "energas_1000", "energas_900", "extra_sushi", "extra_pudim", "extra_temaki", "extra_agua" ];
-let OptPrices = [ 1000, 700, 900, 1000, 900, 800, 450, 600, 700];
-
+let OptPrices = [ 1000, 700, 900, 1000, 900, 800, 600, 700, 450];
 let BtnNamesRem = [ "combo_sushi_rem", "combo_pudim_rem", "combo_temaki_rem", "energas_1000_rem", "energas_900_rem", "extra_sushi_rem", "extra_pudim_rem", "extra_temaki_rem", "extra_agua_rem" ];
 let BtnNamesAdd = [ "combo_sushi_add", "combo_pudim_add", "combo_temaki_add", "energas_1000_add", "energas_900_add", "extra_sushi_add", "extra_pudim_add", "extra_temaki_add", "extra_agua_add" ];
+let dollarUSLocale = Intl.NumberFormat('en-US');
+
 
 $(() => {
 
@@ -44,6 +45,16 @@ $(() => {
             }
         }     
     });
+
+    //BOTAO COPIAR
+    document.getElementById("txtCopy").addEventListener("click", ()=> {
+        let storage = [];
+        for (let index = 0; index < OptNames.length; index++) { storage[index] = rDataString(OptNames[index]) > 0 ? (`${rDataString(OptNames[index])} ${OptNames[index].replace("_", " ")} | `): ""; }
+        let strTxt = storage[0] + storage[1] + storage[2] + storage[3] + storage[4]+ storage[5]+ storage[6]+ storage[7]+ storage[8];
+        let vFinal = (rDataString(OptNames[0]) * OptPrices[0]) + (rDataString(OptNames[1]) * OptPrices[1]) + (rDataString(OptNames[2]) * OptPrices[2]) + (rDataString(OptNames[3]) * OptPrices[3]) + (rDataString(OptNames[4]) * OptPrices[4]) + (rDataString(OptNames[5]) * OptPrices[5]) + (rDataString(OptNames[6]) * OptPrices[6]) + (rDataString(OptNames[7]) * OptPrices[7]) + (rDataString(OptNames[8]) * OptPrices[8]);
+        let vString = (`${strTxt.slice(0, strTxt.length - 3)} // Total: R$${dollarUSLocale.format(vFinal)} || 50%: R$${dollarUSLocale.format((vFinal / 2))}`);
+        navigator.clipboard.writeText(vString);
+    });
 });
 
 function rDataString(value){
@@ -52,8 +63,8 @@ function rDataString(value){
 }
 
 function script(){
-    for (let index = 0; index < OptNames.length; index++) { document.getElementById(OptNames[index]).value = localStorage.getItem(OptNames[index]); }
-    for (let index = 0; index < OptNames.length; index++) {
+    for (let index = 0; index < OptNames.length; index++) { 
+        document.getElementById(OptNames[index]).value = localStorage.getItem(OptNames[index]);
         localStorage.setItem(OptNames[index], rDataString(OptNames[index]));
         if (rDataString(OptNames[index]) > 0) { document.getElementById(OptNames[index]).style.color = "red"; }
         else { document.getElementById(OptNames[index]).style.color = "grey"; }
@@ -62,10 +73,9 @@ function script(){
 }
 
 function setValueFinal(){
-    let dollarUSLocale = Intl.NumberFormat('en-US');
-    let valor_final = (rDataString("combo_sushi") * OptPrices[0]) + (rDataString("combo_pudim") * OptPrices[1]) + (rDataString("combo_temaki") * OptPrices[2]) + (rDataString("energas_1000") * OptPrices[3]) + (rDataString("energas_900") * OptPrices[4]) + (rDataString("extra_sushi") * OptPrices[5]) + (rDataString("extra_agua") * OptPrices[6]) + (rDataString("extra_pudim") * OptPrices[7]) + (rDataString("extra_temaki") * OptPrices[8]);
-    document.getElementById("preco_final").innerHTML = "TOTAL: R$" + dollarUSLocale.format(valor_final);
-    document.getElementById("preco_metade").innerHTML = "METADE: R$" + dollarUSLocale.format((valor_final / 2));
+    let vFinal = (rDataString(OptNames[0]) * OptPrices[0]) + (rDataString(OptNames[1]) * OptPrices[1]) + (rDataString(OptNames[2]) * OptPrices[2]) + (rDataString(OptNames[3]) * OptPrices[3]) + (rDataString(OptNames[4]) * OptPrices[4]) + (rDataString(OptNames[5]) * OptPrices[5]) + (rDataString(OptNames[6]) * OptPrices[6]) + (rDataString(OptNames[7]) * OptPrices[7]) + (rDataString(OptNames[8]) * OptPrices[8]);
+    document.getElementById("preco_final").innerHTML = "TOTAL: R$" + dollarUSLocale.format(vFinal);
+    document.getElementById("preco_metade").innerHTML = "METADE: R$" + dollarUSLocale.format((vFinal / 2));
 }
 
 function SetInpColor(){
